2d43985c38c24984e6088e686ade97567e50c0a4
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2021-01-04 16:50:35 -0300

    created migrations to be executed by flyway

---
+++ dsdeliver/src/main/resources/db/migration/V20210104163400__insert_example_data.sql
@@ -0,0 +1,32 @@
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (1, 'Pizza Bacon', 49.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_bacon.jpg', 'Pizza de bacon com mussarela, orégano, molho especial e tempero da casa.');
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (2, 'Pizza Moda da Casa', 59.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_moda.jpg', 'Pizza à moda da casa, com molho especial e todos ingredientes básicos, e queijo à sua escolha.');
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (3, 'Pizza Portuguesa', 45.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_portuguesa.jpg', 'Pizza Portuguesa com molho especial, mussarela, presunto, ovos e especiarias.');
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (4, 'Risoto de Carne', 52.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_carne.jpg', 'Risoto de carne com especiarias e um delicioso molho de acompanhamento.');
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (5, 'Risoto Funghi', 59.95, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_funghi.jpg', 'Risoto Funghi feito com ingredientes finos e o toque especial do chef.');
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (6, 'Macarrão Espaguete', 35.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_espaguete.jpg', 'Macarrão fresco espaguete com molho especial e tempero da casa.');
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (7, 'Macarrão Fusili', 38.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_fusili.jpg', 'Macarrão fusili com toque do chef e especiarias.');
+INSERT INTO tb_product (id, name, price, image_uri, description) VALUES (8, 'Macarrão Penne', 37.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_penne.jpg', 'Macarrão penne fresco ao dente com tempero especial.');
+
+INSERT INTO tb_order (id, status, lat, lon, address, moment) VALUES (1, 'PENDING', -23.561680, -46.656139, 'Avenida Paulista, 1500', now());
+INSERT INTO tb_order (id, status, lat, lon, address, moment) VALUES (2, 'DELIVERED', -22.946779, -43.217753, 'Avenida Paulista, 1500', now());
+INSERT INTO tb_order (id, status, lat, lon, address, moment) VALUES (3, 'PENDING', -25.439787, -49.237759, 'Avenida Paulista, 1500', now());
+INSERT INTO tb_order (id, status, lat, lon, address, moment) VALUES (4, 'PENDING', -23.561680, -46.656139, 'Avenida Paulista, 1500', now());
+INSERT INTO tb_order (id, status, lat, lon, address, moment) VALUES (5, 'DELIVERED', -23.561680, -46.656139, 'Avenida Paulista, 1500', now());
+INSERT INTO tb_order (id, status, lat, lon, address, moment) VALUES (6, 'PENDING', -23.561680, -46.656139, 'Avenida Paulista, 1500', now());
+INSERT INTO tb_order (id, status, lat, lon, address, moment) VALUES (7, 'PENDING', -23.561680, -46.656139, 'Avenida Paulista, 1500', now());
+
+INSERT INTO tb_order_product (order_id, product_id) VALUES (1 , 1);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (1 , 4);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (2 , 2);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (2 , 5);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (2 , 8);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (3 , 3);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (3 , 4);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (4 , 2);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (4 , 6);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (5 , 4);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (5 , 6);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (6 , 5);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (6 , 1);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (7 , 7);
+INSERT INTO tb_order_product (order_id, product_id) VALUES (7 , 5);
