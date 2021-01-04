1f93d80ccdbeaca2948497427759ea1acf99581c
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2021-01-04 14:22:23 -0300

    created dsdeliver project

---
+++ dsdeliver/src/test/java/com/jetherrodrigues/sds2/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetherrodrigues.sds2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
